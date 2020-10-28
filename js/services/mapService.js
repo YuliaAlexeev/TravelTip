0d8aa06815487382783d95e545c7aa4b469964ce
Shimrit Herbst <shimrit.levine@gmail.com>
2020-10-28 20:27:47 +0200

    Added onClickMap func

--- js/services/mapService.js
+++ js/services/mapService.js
@@ -4,12 +4,12 @@ import { storageService } from '../storage-service.js';
 const STORAGE_KEY = 'placesData';
 var gPlacesData = createPlaces();
 
-function createPlace(lat, lng, name){
+function createPlace(lat, lng, name) {
     return {
-        id: 1, 
-        name, 
-        lat, 
-        lng, 
+        id: 1,
+        name,
+        lat,
+        lng,
         // weather, 
         // createdAt, 
         // updatedAt
@@ -22,9 +22,9 @@ function createPlaces(){
 }
 
 function askLocation(location) {
-    
+
     const prmRes = axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyDGypLOJoL1NOMoJRqBiGsUZa7aRlA0Snk`);
-    
+
     const prmLoc = prmRes.then(res => {
         console.log('res', res.data);
         const { lat, lng } = res.data.results[0].geometry.location;
@@ -55,3 +55,26 @@ function getLocs() {
         }, 2000);
     });
 }
+
+// function onAddPlace(positionName, positionOnMap){
+//     let place =  createPlace(positionOnMap.lat, positionOnMap.lng, positionName);
+//     gUserData.places.push(place);
+//     _saveUserToStorage();
+// }
+
+// function removePlace(id){
+//     var placeIdx = gUserData.places.findIndex(place => id === place.id);
+//     gUserData.places.splice(placeIdx, 1)
+//     _saveUserToStorage();
+// }
+
+// function createPlaces(){
+//     let placesData = loadFromStorage(STORAGE_PLACES_KEY) || createPlace();
+//     return placesData;
+// }
+
+
+export const mapService = {
+    getLocs: getLocs,
+    askLocation: askLocation
+};
